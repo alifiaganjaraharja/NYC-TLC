a131acbe574f8af875a46da534a65e93af1b1ef8
Alifia Ganjaraharja <121094831+alifiaganjaraharja@users.noreply.github.com>
2025-11-05 12:07:03 +0700

    Add files via upload

---
+++ queries/[Question1]-BusiestHours-GreenTaxi.sql
@@ -0,0 +1,22 @@
+SELECT
+  -- 1. Create a new category for Weekday and Weekend
+  CASE WHEN EXTRACT(DAYOFWEEK FROM pickup_datetime) IN (1,7) THEN 'Weekend'
+  ELSE 'Weekday'
+  END AS day_type,
+
+  -- 2. Extract the pickup hour from the green taxi pickup time
+  EXTRACT(HOUR FROM pickup_datetime) AS pickup_hour,
+
+  -- 3. Count all trips
+  COUNT(*) AS total_pickups
+FROM 
+  -- This line queries all tables that start with 'tlc_green_trips'
+`bigquery-public-data.new_york_taxi_trips.tlc_green_trips_*`
+GROUP BY 
+  -- 4. Group by BOTH the new category and the pickup_hour
+  day_type, 
+  pickup_hour
+ORDER BY
+  -- 5. Sort by day type, then by the busiest hours
+  day_type,
+  total_pickups DESC;
