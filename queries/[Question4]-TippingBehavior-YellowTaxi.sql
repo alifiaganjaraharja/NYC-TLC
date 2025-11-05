cbe786183305537fe99629f0e833aff617d631b3
Alifia Ganjaraharja <121094831+alifiaganjaraharja@users.noreply.github.com>
2025-11-05 12:27:40 +0700

    Add files via upload

---
+++ queries/[Question4]-TippingBehavior-YellowTaxi.sql
@@ -0,0 +1,13 @@
+SELECT 
+  payment_type,
+  COUNT(*) AS trip_count,
+  ROUND(AVG(tip_amount), 2) AS avg_tip,
+  ROUND(AVG(fare_amount), 2) AS avg_fare,
+  ROUND(AVG(tip_amount / NULLIF(fare_amount, 0) * 100), 2) AS avg_tip_percentage,
+FROM -- This line queries all tables that start with 'tlc_yellow_trips'
+`bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_*`
+WHERE payment_type IN ('1', '2')  -- Only these two (Cash vs. Credit Card)
+  AND pickup_location_id IS NOT NULL 
+  AND dropoff_location_id IS NOT NULL
+  AND fare_amount > 0
+GROUP BY payment_type;
